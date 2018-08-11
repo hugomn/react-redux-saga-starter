fdd03351f1a02b72298e6a19f7becd13cb0d9d82
Hugo Magalhães <hugomn@gmail.com>
2018-08-11 03:43:47 +0200

    Add Semantic-ui-react lib for UI components

--- src/components/Question/template.js
+++ src/components/Question/template.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import { Card, Grid, Container, Header } from "semantic-ui-react";
 
 class QuestionList extends Component {
   static propTypes = {
@@ -11,12 +12,32 @@ class QuestionList extends Component {
     this.props.fetchQuestions();
   };
 
+  renderQuestion = question => {
+    return (
+      <Card fluid>
+        <Card.Content>
+          <Card.Header>{question.question}</Card.Header>
+          <Card.Meta>{question.published_at} </Card.Meta>
+          <Card.Description>{question.choices.length} choices</Card.Description>
+        </Card.Content>
+      </Card>
+    );
+  };
+
   render() {
     const { questions } = this.props;
     return (
       <React.Fragment>
-        <h1>Questions</h1>
-        {questions.length}
+        <Container>
+          <Header as="h1">Questions</Header>
+          <Grid stackable>
+            {questions.map(question => (
+              <Grid.Column computer={4} tablet={8} mobile={16}>
+                {this.renderQuestion(question)}
+              </Grid.Column>
+            ))}
+          </Grid>
+        </Container>
       </React.Fragment>
     );
   }
