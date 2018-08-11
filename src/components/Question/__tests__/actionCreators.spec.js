edeb9f23384b389d3c757a5a8f0f8d8586bd0947
Hugo Magalhães <hugomn@gmail.com>
2018-08-11 19:23:05 +0200

    Add coverage tests for action creators

--- src/components/Question/__tests__/actionCreators.spec.js
+++ src/components/Question/__tests__/actionCreators.spec.js
@@ -1,14 +1,75 @@
-import {
-  fetchQuestionsRequested,
-  fetchQuestionsSucceeded,
-  fetchQuestionsFailed
-} from "components/Question/actionCreators";
+import * as actionCreators from "components/Question/actionCreators";
 import * as actions from "components/Question/actions";
 
 describe("Question actionCreators", () => {
+  it("choiceSelected should return the correct action type", () => {
+    expect(actionCreators.choiceSelected().type).toEqual(
+      actions.QUESTION_CHOICE_SELECTED
+    );
+  });
+
   it("fetchQuestionsRequested should return the correct action type", () => {
-    expect(fetchQuestionsRequested.type).toEqual(
-      actions.FETCH_QUESTIONS_REQUESTED
+    expect(actionCreators.fetchQuestionRequested().type).toEqual(
+      actions.QUESTION_FETCH_REQUESTED
+    );
+  });
+
+  it("fetchQuestionsRequested should return the correct action type", () => {
+    expect(actionCreators.fetchQuestionsRequested().type).toEqual(
+      actions.QUESTIONS_FETCH_REQUESTED
+    );
+  });
+
+  it("fetchQuestionSucceeded should return the correct action type", () => {
+    expect(actionCreators.fetchQuestionSucceeded().type).toEqual(
+      actions.QUESTION_FETCH_SUCCEEDED
+    );
+  });
+
+  it("fetchQuestionsSucceeded should return the correct action type", () => {
+    expect(actionCreators.fetchQuestionsSucceeded().type).toEqual(
+      actions.QUESTIONS_FETCH_SUCCEEDED
+    );
+  });
+
+  it("fetchQuestionFailed should return the correct action type", () => {
+    expect(actionCreators.fetchQuestionFailed().type).toEqual(
+      actions.QUESTION_FETCH_FAILED
+    );
+  });
+
+  it("fetchQuestionsFailed should return the correct action type", () => {
+    expect(actionCreators.fetchQuestionsFailed().type).toEqual(
+      actions.QUESTIONS_FETCH_FAILED
+    );
+  });
+
+  it("choiceVoteFailed should return the correct action type", () => {
+    expect(actionCreators.choiceVoteFailed().type).toEqual(
+      actions.QUESTION_CHOICE_VOTE_FAILED
+    );
+  });
+  it("choiceVoteRequested should return the correct action type", () => {
+    expect(actionCreators.choiceVoteRequested().type).toEqual(
+      actions.QUESTION_CHOICE_VOTE_REQUESTED
+    );
+  });
+
+  it("choiceSelected should return the correct action type", () => {
+    expect(actionCreators.choiceSelected().type).toEqual(
+      actions.QUESTION_CHOICE_SELECTED
+    );
+  });
+
+  it("choiceVoteSucceeded should return the correct action type", () => {
+    expect(actionCreators.choiceVoteSucceeded().type).toEqual(
+      actions.QUESTION_CHOICE_VOTE_SUCEEDED
+    );
+  });
+
+  it("showDetail should return the correct action type", () => {
+    expect(actionCreators.showDetail().type).toEqual(
+      actions.QUESTION_SHOW_DETAIL
     );
   });
 });
