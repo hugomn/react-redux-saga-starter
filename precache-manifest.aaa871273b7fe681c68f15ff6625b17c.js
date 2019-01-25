bee2b388918983018f11d516a7994ed941a37fd4
Hugo <hugomn@gmail.com>
2019-01-25 13:47:07 +0000

    Automated deployment to GitHub Pages: 1b273dfa2db6ed06f2056df67762743690d98176

---
+++ precache-manifest.aaa871273b7fe681c68f15ff6625b17c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ef6a5cb1bc123bbed0a6",
+    "url": "/static/css/main.55e37e12.chunk.css"
+  },
+  {
+    "revision": "ef6a5cb1bc123bbed0a6",
+    "url": "/static/js/main.ef6a5cb1.chunk.js"
+  },
+  {
+    "revision": "af2c2c1b6b0bbe03805c",
+    "url": "/static/js/1.af2c2c1b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6e37bafaa0a41c5f6da37f063d18d87a",
+    "url": "/index.html"
+  }
+];
