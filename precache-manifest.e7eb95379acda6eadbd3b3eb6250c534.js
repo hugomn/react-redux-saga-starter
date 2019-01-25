0c7285b282fbd48362400ac1b786f3d6c5218386
Hugo <hugomn@gmail.com>
2019-01-25 13:56:26 +0000

    Automated deployment to GitHub Pages: d32b34868b20560ae01f52f3022691a0950cc0c2

---
+++ precache-manifest.e7eb95379acda6eadbd3b3eb6250c534.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ef6a5cb1bc123bbed0a6",
+    "url": "./static/css/main.55e37e12.chunk.css"
+  },
+  {
+    "revision": "ef6a5cb1bc123bbed0a6",
+    "url": "./static/js/main.ef6a5cb1.chunk.js"
+  },
+  {
+    "revision": "af2c2c1b6b0bbe03805c",
+    "url": "./static/js/1.af2c2c1b.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "cb955332c106b74f5dfd79a058d9f06f",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.aaa871273b7fe681c68f15ff6625b17c.js"
+  "./precache-manifest.e7eb95379acda6eadbd3b3eb6250c534.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
